a7738ddf06401570ca826c81209684a1d5638085
apeng-singlestore <127370261+apeng-singlestore@users.noreply.github.com>
2024-09-13 17:49:59 -0700

    add planetscale mode to allow for tests with schema

--- integration-tests/tests/relational/issues-schemas/duplicates/singlestore/singlestore.duplicates.test.ts
+++ integration-tests/tests/relational/issues-schemas/duplicates/singlestore/singlestore.duplicates.test.ts
@@ -68,7 +68,7 @@ beforeAll(async () => {
 
 	await client.query(`CREATE DATABASE IF NOT EXISTS drizzle;`);
 	await client.changeUser({ database: 'drizzle' });
-	db = drizzle(client, { schema, logger: ENABLE_LOGGING });
+	db = drizzle(client, { schema, logger: ENABLE_LOGGING, mode: 'planetscale' });
 });
 
 afterAll(async () => {
